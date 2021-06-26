a995353ed27868b5f1e42ad3b9f861aa017393a1
Hariket Sukeshkumar Sheth <hariket.sukeshkumar2020@gmail.com>
2021-06-26 09:20:55 +0530

    Added IPS20_Ques1.cpp

---
+++ IPS 20/IPS20_Ques1.cpp
@@ -0,0 +1,49 @@
+/*
+STL_Sort Strings
+Write a C++ Program and Subsequent pseudocode to sort the given list of 'N' strings using STL(Arrays/Vectors/List) of your choice. Also, display the
+names in alphabetical order without duplication.
+
+Input Format:
+Read n
+Read Names
+
+
+Output Format:
+Display the names in alphabetical order without duplication
+
+Note : if the value of N is -ve, Print "Invalid Input"
+*/ 
+  
+/*
+Name: Hariket Sukeshkumar Sheth
+Topic: STL (Standard Template Library) in C++
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    
+    int num;
+    string name;
+    cin>>num;
+    
+    if(num<0)
+        cout<<"Invalid Input"<<endl;
+        
+    else{
+        vector<string> vc;
+        vector<string> :: iterator it;
+        for(int i=0; i<num; i++){
+            cin>>name;
+            it = find(vc.begin(),vc.end(),name);
+            if(it == vc.end())
+                vc.push_back(name);
+        }
+        
+        sort(vc.begin(), vc.end());
+        for(it = vc.begin(); it != vc.end(); ++it)
+            cout << *it << endl;
+    }
+}
